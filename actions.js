2b079079241fb010e2f6fe19582fb33ad055f4e7
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-07-09 02:18:50 -0500

    Mobile View Complete, Side Menu Works&Counter Cart

---
+++ actions.js
@@ -0,0 +1,20 @@
+// Open/Close Menu
+const toggleMenu = () => (document.body.classList.toggle("open"));
+
+// Cart counter
+let spanCount = document.querySelector('.quantity-product');
+let quantityProduct = 0;
+
+const addCounter = () => {
+    quantityProduct++;
+    spanCount.textContent = quantityProduct;
+}
+
+const removeCounter = () => {
+    quantityProduct--;
+    if (quantityProduct >= 0) {
+        spanCount.textContent = quantityProduct;
+    } else {
+        spanCount.textContent = 0;
+    }
+}
